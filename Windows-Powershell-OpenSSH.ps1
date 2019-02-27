bdba8e7b2f7f43a517c1052cae9f754785e07a9c
housni.h <abdel.housni@gmail.com>
2019-02-27 20:01:12 +0100

    Update Windows-Powershell-OpenSSH.ps1

--- Windows-Powershell-OpenSSH.ps1
+++ Windows-Powershell-OpenSSH.ps1
@@ -1,3 +1,14 @@
-# https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
-# http://garrettyamada.com/powershell-remoting/
+# Installing OpenSSH with PowerShell
+# Tested with Windows 10 1809
+# source : https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
+# source : http://garrettyamada.com/powershell-remoting/
 
+
+# Verify if OpenSSH Client/Server available/installed
+Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
+
+# Install the OpenSSH Client
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+
+# Install the OpenSSH Server
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
