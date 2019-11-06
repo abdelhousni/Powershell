0bc770260d65bb2c22851604a23a825c3900d323
housni.h <23284113+AbdellatifHousni@users.noreply.github.com>
2019-11-06 13:01:24 +0100

    Create get-server-printers-name-ip-address.ps1

---
+++ get-server-printers-name-ip-address.ps1
@@ -0,0 +1,4 @@
+#source : https://powershell.org/forums/topic/get-name-and-ip-address-of-a-printer/
+
+$print_server = 'server-printer'
+Get-WMIObject -Class Win32_Printer -Computer $print_server | %{ $printer = $_.Name; $port = $_.portname; gwmi win32_tcpipprinterport -computername $print_server | where { $_.Name -eq $port } | select @{name="printername";expression={$printer}}, hostaddress }
