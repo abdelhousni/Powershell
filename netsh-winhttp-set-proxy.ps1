e30e1caaafa3b88f0803002c7bc08a0bf6729bfa
housni.h <23284113+AbdellatifHousni@users.noreply.github.com>
2019-11-13 12:27:13 +0100

    Create netsh-winhttp-set-proxy.ps1
    Windows - Netsh Winhttp set proxy server parameters (for windows update etc...)

---
+++ netsh-winhttp-set-proxy.ps1
@@ -0,0 +1,5 @@
+# Abdellatif Housni - 2019
+# Windows - Netsh Winhttp set proxy server parameters (for windows update etc...)
+$proxy_server = "http://proxy.acme.com:3128"
+$bypass_list = "<local>;172.22.*;192.168.*;*.localdomain.lan"
+netsh winhttp set proxy proxy-server=$proxy_server bypass-list=$bypass_list
