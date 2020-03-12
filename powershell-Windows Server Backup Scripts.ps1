4f253c84e6f1006e095b1b8bfea00fc3d44f9319
housni.h <23284113+AbdellatifHousni@users.noreply.github.com>
2020-03-12 08:37:24 +0100

    Create powershell-Windows Server Backup Scripts.ps1

---
+++ powershell-Windows Server Backup Scripts.ps1
@@ -0,0 +1,12 @@
+
+## source : [Windows Server 2008 R2 PowerShell Backup](https://social.technet.microsoft.com/wiki/contents/articles/2216.windows-server-2008-r2-powershell-backup.aspx)
+# Windows 2008 - Enable Windows PowerShell Support for Windows Server Backup
+Import-Module ServerManager 
+Add-WindowsFeature Backup-Features -Include 
+Add-PSSnapin Windows.ServerBackup
+# Create /Get/Edit Policy (Schedule) 
+# The backup is controlled by the WBPolicy. This policy includes all the information necessary to perform a backup, including the backup schedule. You can create a new policy, retrieve an existing policy, or open an existing policy in edit mode to change it.
+$BPol = Get-WBPolicy 
+$BPol = Get-WBPolicy -edit  
+$BPol = New-WBPolicy
+
