8c88b915f23cc2646befe9b733a354c66dde8160
housni.h <23284113+AbdellatifHousni@users.noreply.github.com>
2020-12-27 17:37:33 +0100

    Update powershell-visio-automation.ps1

--- powershell-visio-automation.ps1
+++ powershell-visio-automation.ps1
@@ -1,7 +1,6 @@
 # Visio automation via PowerShell - stackoverflow.com
 # source : https://stackoverflow.com/questions/14730083/visio-automation-via-powershell?rq=1
 
-
 $application = New-Object -ComObject Visio.Application
 $application.visible = $true
 $documents = $application.Documents
@@ -9,11 +8,10 @@ $document = $documents.Add("Basic Network Diagram.vst")
 $pages = $application.ActiveDocument.Pages
 $page = $pages.Item(1)
 
-
 $ComputerStencil = $application.Documents.Add("Computers and Monitors.vss")
-
-
 
 $pc = $ComputerStencil.Masters.Item("PC")
 $shape1 = $page.Drop($pc, 2.2, 6.8)
 $shape1.Text = "Some text...."
+
+$document.
